5012b13a3b1c40c91398141f71fa92980f89d39d
zaki1993 <zaki1993@abv.bg>
2016-08-02 11:44:55 +0300

    threads starting

--- WebServerCpp/server.cpp
+++ WebServerCpp/server.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>      
-#include <sys/socket.h> 
+#include <sys/socket.h> //for socket connection, etc.
 #include <netdb.h>      
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,11 +9,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <netinet/in.h> 
-#include <fstream>
-#include <dirent.h>
+#include <fstream> //reading and writing to files
+#include <dirent.h> //get all the folder content
 #include <sys/stat.h>
-#include <math.h>
-#include <pthread.h>
+#include <math.h>//used in intToString function
+#include <pthread.h>//for multythreading
 
 
 //declaring some variables
@@ -53,7 +53,7 @@ const char *response_400 = "HTTP/1.1 400 Bad Request\nContent-Type: text/html; c
 const char *response_404 = "HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=utf-8\n\n<html><body><i>Not Found!</i></body></html>";
 
 void NotFoundErr(int &cliSocket,ssize_t sent_bytes){ //404 NOT FOUND
-	     			bytes_sent = write(new_sd,response_404,strlen(response_404));
+	bytes_sent = write(new_sd,response_404,strlen(response_404));
 }
 
 void getFolderContent(std::string path){
@@ -122,6 +122,7 @@ std::string convertIntToString(int number){
 	length++;
 	}
 	std::string result="";
+	if(!length){ result+="0"; return result; }
 	while(length!=0){
 		result += char(48+number/int(pow(10,length-1)));
 		number = number%int(pow(10,length-1));
@@ -129,76 +130,7 @@ std::string convertIntToString(int number){
 	}
 return result;
 }
-int main(int argc, char *argv[])
-{	
-    int status;
-    struct addrinfo host_info;       
-    struct addrinfo *host_info_list; 
-    const char* port = argv[1];
-    memset(&host_info, 0, sizeof host_info);
-
-    std::cout << "Setting up the structs..."  << std::endl;
-
-    host_info.ai_family = AF_UNSPEC;     
-    host_info.ai_socktype = SOCK_STREAM; 
-    host_info.ai_flags = AI_PASSIVE;    
-
-    status = getaddrinfo(NULL, port, &host_info, &host_info_list); //127.0.0.1 = NULL
-    
-    if (status != 0){ std::cout<<"Could not connect to port: "<<port<<std::endl; return 0; }
-
-
-    std::cout << "Creating a socket..."  << std::endl;
-    int socketfd ; // The socket descripter
-    socketfd = socket(host_info_list->ai_family, host_info_list->ai_socktype, host_info_list->ai_protocol);
-    if (socketfd == -1)  std::cout << "socket error " ;
-
-    std::cout << "Binding socket..."  << std::endl;
-    
-    status = setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, (char*)true, sizeof(bool));
-    status = bind(socketfd, host_info_list->ai_addr, host_info_list->ai_addrlen);
-	if(status<0){
-
-	std::cout<<"Socket error "<<std::endl;
-	std::cout << "Stopping server..." << std::endl;
-	freeaddrinfo(host_info_list);	
-	return 0;
-	}
-    std::cout << "Listen()ing for connections..."  << std::endl;
-    status =  listen(socketfd, 5);
-
-	while(1){
-	bytes_recieved = 0;
-	totalBytesRead = 0;
-	fullPath="";
-	addrcontainer = "";
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size = sizeof(their_addr);
-    new_sd = accept(socketfd, (struct sockaddr *)&their_addr, &addr_size);
-    if (new_sd == -1)
-    {
-        std::cout << "listen error" << std::endl ;
-    }
-    else
-    {
-        std::cout << "Connection accepted. Using new socketfd : "  <<  new_sd << std::endl;
-    }
-
-    std::cout<<"Buffer size: "<<sizeof(incomming_data_buffer)<<std::endl;
-    std::cout << "Waiting to recieve data..."  << std::endl;
-
-    do{
-    bytes_recieved = recv(new_sd, incomming_data_buffer,buffersize, 0);
-    totalBytesRead +=bytes_recieved;
-    if (bytes_recieved == 0){ std::cout<<std::endl << "The server is waiting for your request..!" << std::endl;  close(new_sd);}
-    if (bytes_recieved == -1){ std::cout<<std::endl << "recieve error!" << std::endl;  close(new_sd);}
-
-  for(int i = 0;i<bytes_recieved;i++){
-    std::cout <<incomming_data_buffer[i];
-	fullPath+=incomming_data_buffer[i];
-        }
-}while(bytes_recieved == buffersize);
-     std::cout << totalBytesRead << " bytes recieved" << std::endl;
+void staticFiles(){
 
 		  for(int i = 5;fullPath[i]!=' ';i++){;
 			addrcontainer+=fullPath[i]; //get the exact address
@@ -221,7 +153,7 @@ int main(int argc, char *argv[])
 				}
 				std::string headers = "HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n";
 				bytes_sent = write(new_sd, headers.data(), headers.length());
-				if(a=="" || b =="" || result[strlen(result)-1]<char(48) || result[strlen(result)-1]>char(57)){
+				if(strlen(a.c_str())==0 || strlen(b.c_str())==0 || result[strlen(result)-1]<char(48) || result[strlen(result)-1]>char(57)){
 					bytes_sent = write(new_sd,"Incorrect input..!",strlen("Incorrect input..!"));
 				}
 				else{
@@ -287,6 +219,82 @@ int main(int argc, char *argv[])
 		}
 		delete []result;
 	}
+}
+int main(int argc, char *argv[])
+{	
+    int status;
+    struct addrinfo host_info;       
+    struct addrinfo *host_info_list;
+    const char* port = argv[1];
+    memset(&host_info, 0, sizeof host_info);
+
+    std::cout << "Setting up the structs..."  << std::endl;
+
+    host_info.ai_family = AF_UNSPEC;     
+    host_info.ai_socktype = SOCK_STREAM; 
+    host_info.ai_flags = AI_PASSIVE;    
+
+    status = getaddrinfo(NULL, port, &host_info, &host_info_list); //127.0.0.1 = NULL
+    
+    if (status != 0){ std::cout<<"Could not connect to port: "<<port<<std::endl; return 0; }
+
+
+    std::cout << "Creating a socket..."  << std::endl;
+    int socketfd ; // The socket descripter
+    socketfd = socket(host_info_list->ai_family, host_info_list->ai_socktype, host_info_list->ai_protocol);
+    if (socketfd == -1)  std::cout << "socket error " ;
+
+    std::cout << "Binding socket..."  << std::endl;
+    
+    status = setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, (char*)true, sizeof(bool));
+    status = bind(socketfd, host_info_list->ai_addr, host_info_list->ai_addrlen);
+	if(status<0){
+
+	std::cout<<"Socket error "<<std::endl;
+	std::cout << "Stopping server..." << std::endl;
+	freeaddrinfo(host_info_list);	
+	return 0;
+	}
+    std::cout << "Listen()ing for connections..."  << std::endl;
+    status =  listen(socketfd, 5);
+	
+	struct sockaddr_storage their_addr;
+	socklen_t addr_size = sizeof(their_addr);
+
+	while(1){
+	bytes_recieved = 0;
+	totalBytesRead = 0;
+	fullPath="";
+	addrcontainer = "";
+    new_sd = accept(socketfd, (struct sockaddr *)&their_addr, &addr_size);
+    if (new_sd == -1)
+    {
+        std::cout << "listen error" << std::endl ;
+    }
+    else
+    {
+        std::cout << "Connection accepted. Using new socketfd : "  <<  new_sd << std::endl;
+    }
+
+    std::cout<<"Buffer size: "<<sizeof(incomming_data_buffer)<<std::endl;
+    std::cout << "Waiting to recieve data..."  << std::endl;
+    
+
+    do{
+    bytes_recieved = recv(new_sd, incomming_data_buffer,buffersize, 0);
+    totalBytesRead +=bytes_recieved;
+    if (bytes_recieved == 0){ std::cout<<std::endl << "The server is waiting for your request..!" << std::endl;  close(new_sd);}
+    if (bytes_recieved == -1){ std::cout<<std::endl << "recieve error!" << std::endl;  close(new_sd);}
+
+  for(int i = 0;i<bytes_recieved;i++){
+    std::cout <<incomming_data_buffer[i];
+	fullPath+=incomming_data_buffer[i];
+        }
+}while(bytes_recieved == buffersize);
+     std::cout << totalBytesRead << " bytes recieved" << std::endl;
+
+     staticFiles();
+
      close(new_sd);
 }
     std::cout << "Stopping server..." << std::endl;
