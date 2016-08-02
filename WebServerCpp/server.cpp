43593a55f70e2752b6fb5447e19426ee00145471
zaki1993 <zaki1993@abv.bg>
2016-08-02 16:46:37 +0300

    Update server.cpp
    threads added

--- WebServerCpp/server.cpp
+++ WebServerCpp/server.cpp
@@ -14,17 +14,25 @@
 #include <sys/stat.h>
 #include <math.h>//used in intToString function
 #include <pthread.h>//for multythreading
+#include <sstream>
+#include <vector>
 
+using namespace std;
 
 //declaring some variables
 const int buffersize = 1024; 
 char incomming_data_buffer[buffersize];
 ssize_t bytes_sent = 0;
 std::string fullPath;
-int new_sd; //the client socket
 ssize_t bytes_recieved = 0;
 ssize_t totalBytesRead = 0;
 std::string addrcontainer = "";
+
+ const char *response_200 = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n<html><body><i>Hello!</i></body></html>";
+    const char *response_400 = "HTTP/1.1 400 Bad Request\nContent-Type: text/html; charset=utf-8\n\n<html><body><i>Bad Request!</i></body></html>";
+    const char *response_404 = "HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=utf-8\n\n<html><body><i>Not Found!</i></body></html>";
+
+
 typedef enum {gif, html, jpeg, jpg, png, plain,mp4, notfound} ext; //all the extensions that the web server recognises
 
 //function that finds the extension of the file
@@ -47,16 +55,11 @@ ext get_ext(char *file) {
 	return notfound;
 	}
 }
-
-const char *response_200 = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n<html><body><i>Hello!</i></body></html>";
-const char *response_400 = "HTTP/1.1 400 Bad Request\nContent-Type: text/html; charset=utf-8\n\n<html><body><i>Bad Request!</i></body></html>";
-const char *response_404 = "HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=utf-8\n\n<html><body><i>Not Found!</i></body></html>";
-
 void NotFoundErr(int &cliSocket,ssize_t sent_bytes){ //404 NOT FOUND
-	bytes_sent = write(new_sd,response_404,strlen(response_404));
+	bytes_sent = write(cliSocket,response_404,strlen(response_404));
 }
 
-void getFolderContent(std::string path){
+void getFolderContent(std::string path,int &cliSocket){
 	std::ifstream fin;
 	int num;
 	struct dirent *dirp;
@@ -73,42 +76,42 @@ void getFolderContent(std::string path){
 	// Endeavor to read a single number from the file and display it
 	    fin.open( filepath.c_str());
 	    if (fin >> num)
-		bytes_sent = write(new_sd,"<br>\n",strlen("<br>\n"));
-	       	bytes_sent = write(new_sd,filepath.c_str(),strlen(filepath.c_str()));
-		bytes_sent = write(new_sd,"</br>\n",strlen("</br>\n"));
+		bytes_sent = write(cliSocket,"<br>\n",strlen("<br>\n"));
+	       	bytes_sent = write(cliSocket,filepath.c_str(),strlen(filepath.c_str()));
+		bytes_sent = write(cliSocket,"</br>\n",strlen("</br>\n"));
 	    fin.close();
 	    }
 	//close
 	  closedir( dp );
 }
-void htmlReader(char* path){
+void htmlReader(char* path,int &cliSocket){
 			std::ifstream file(path);
 			if(file.fail()){
-				NotFoundErr(new_sd,bytes_sent);
+				NotFoundErr(cliSocket,bytes_sent);
 			}
 			else{
 			std::string temp = "";
 			std::string headers = "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n";
-			bytes_sent = write(new_sd, headers.data(), headers.length());
+			bytes_sent = write(cliSocket, headers.data(), headers.length());
 			
 			while (std::getline(file,temp)) {
-				   bytes_sent = write(new_sd,temp.c_str(),strlen(temp.c_str()));
+				   bytes_sent = write(cliSocket,temp.c_str(),strlen(temp.c_str()));
 			}
 				file.close();
 			}
 }
-void imageReader(char* result,std::string type,std::string subtype){
+void imageReader(char* result,std::string type,std::string subtype,int &cliSocket){
 std::ifstream file(result,std::ios::in | std::ios::binary);
 	if(file.fail()){
-		NotFoundErr(new_sd,bytes_sent);
+		NotFoundErr(cliSocket,bytes_sent);
 	}
 	else{
 	std::string headers = "HTTP/1.0 200 OK\r\nContent-type: "+type+"/"+subtype+"\r\n\r\n";
-	bytes_sent = write(new_sd, headers.data(), headers.length());
+	bytes_sent = write(cliSocket, headers.data(), headers.length());
 	
 	while(file.tellg()!=-1){
 		file.read(incomming_data_buffer,buffersize);
-		bytes_sent = write(new_sd,incomming_data_buffer,buffersize);
+		bytes_sent = write(cliSocket,incomming_data_buffer,buffersize);
 	}
 		file.close();
 	}
@@ -130,7 +133,7 @@ std::string convertIntToString(int number){
 	}
 return result;
 }
-void staticFiles(){
+   void staticFiles(int &new_sd){
 
 		  for(int i = 5;fullPath[i]!=' ';i++){;
 			addrcontainer+=fullPath[i]; //get the exact address
@@ -139,8 +142,12 @@ void staticFiles(){
 			int lengthRes = strlen(addrcontainer.c_str());
 			char *result = new char[lengthRes+1];
 			strcpy(result,addrcontainer.c_str());
+		if(strstr(addrcontainer.c_str(),"-download-file")!=NULL){
+			std::string headers = "HTTP/1.0 200 OK\r\nContent-type: application/force-download\r\n\r\n";
+			bytes_sent = write(new_sd, headers.data(), headers.length());
+		}
 		if(get_ext(result)==0){ //reading gif images
-			imageReader(result,"image","gif");
+			imageReader(result,"image","gif",new_sd);
 		}
 		if(get_ext(result)==1){	//reading html files
 			if(strstr(result, "field-a=") != NULL && strstr(result, "field-b=")!=NULL){
@@ -162,17 +169,17 @@ void staticFiles(){
 				}
 			}
 			else{
-				htmlReader(result);
+				htmlReader(result,new_sd);
 			}
 		}
 		if(get_ext(result)==4){ //reading png images
-			imageReader(result,"image","png");
+			imageReader(result,"image","png",new_sd);
 		}
 		if( get_ext(result) == 3){ //reading jpg images
-			imageReader(result,"image","jpg");
+			imageReader(result,"image","jpg",new_sd);
 		}
 		if(get_ext(result)==2){ //reading jpeg images
-			imageReader(result,"image","jpeg");
+			imageReader(result,"image","jpeg",new_sd);
 		}
 		if(get_ext(result)==5){ //reading txt files
 				std::ifstream file(result);
@@ -196,11 +203,11 @@ void staticFiles(){
 					}
 				}
 		if(get_ext(result)==6){ //reading video(mp4) files
-			imageReader(result,"video","mp4");
+			imageReader(result,"video","mp4",new_sd);
 		}	
 		if(get_ext(result)==7){ //folders 
 			if(addrcontainer==""){
-				htmlReader("html/home.html");
+				htmlReader("html/home.html",new_sd);
 			}
 			else{
 				std::ifstream fold(result);
@@ -211,7 +218,7 @@ void staticFiles(){
 					bytes_sent = write(new_sd, headers.data(), headers.length());
 					bytes_sent = write(new_sd,"<!DOCTYPE HTML>\n<html><head><title>Status 200 OK</title></head>\n",strlen("<!DOCTYPE HTML>\n<html><head><title>Status 200 OK</title></head>\n"));
 					bytes_sent = write(new_sd,"<body>\n",strlen("<body>\n"));
-					getFolderContent(result);	
+					getFolderContent(result,new_sd);	
 					bytes_sent = write(new_sd,"</body>\n",strlen("</body>\n"));
 					fold.close();
 				}
@@ -220,12 +227,40 @@ void staticFiles(){
 		delete []result;
 	}
 }
-int main(int argc, char *argv[])
-{	
+void *handle_request(void *pcliefd) 
+{
+    int cliefd = *(int*)pcliefd;
+    delete (int *)pcliefd;
+	bytes_recieved = 0;
+	totalBytesRead = 0;
+	fullPath="";
+	addrcontainer = "";
+    do{
+    bytes_recieved = recv(cliefd, incomming_data_buffer,buffersize, 0);
+    totalBytesRead +=bytes_recieved;
+    if (bytes_recieved == 0){ std::cout<<std::endl << "The server is waiting for your request..!" << std::endl;  close(cliefd);}
+    if (bytes_recieved == -1){ std::cout<<std::endl << "recieve error!" << std::endl;  close(cliefd);}
+
+  for(int i = 0;i<bytes_recieved;i++){
+    std::cout <<incomming_data_buffer[i];
+	fullPath+=incomming_data_buffer[i];
+        }
+}while(bytes_recieved == buffersize);
+     std::cout << totalBytesRead << " bytes recieved" << std::endl;
+	staticFiles(cliefd);
+    close(cliefd);
+    return 0;
+}
+
+int main(int argc, const char *argv[])
+{
+ 	
     int status;
     struct addrinfo host_info;       
     struct addrinfo *host_info_list;
     const char* port = argv[1];
+	 pthread_t thread;
+
     memset(&host_info, 0, sizeof host_info);
 
     std::cout << "Setting up the structs..."  << std::endl;
@@ -257,49 +292,31 @@ int main(int argc, char *argv[])
 	}
     std::cout << "Listen()ing for connections..."  << std::endl;
     status =  listen(socketfd, 5);
-	
-	struct sockaddr_storage their_addr;
-	socklen_t addr_size = sizeof(their_addr);
 
-	while(1){
-	bytes_recieved = 0;
-	totalBytesRead = 0;
-	fullPath="";
-	addrcontainer = "";
-    new_sd = accept(socketfd, (struct sockaddr *)&their_addr, &addr_size);
-    if (new_sd == -1)
-    {
-        std::cout << "listen error" << std::endl ;
-    }
-    else
-    {
-        std::cout << "Connection accepted. Using new socketfd : "  <<  new_sd << std::endl;
-    }
+    struct sockaddr_storage clieaddr;
+    int cliefd;
+    char s[INET_ADDRSTRLEN];
+    socklen_t cliesize;
 
-    std::cout<<"Buffer size: "<<sizeof(incomming_data_buffer)<<std::endl;
-    std::cout << "Waiting to recieve data..."  << std::endl;
-    
+    while(true) {
 
-    do{
-    bytes_recieved = recv(new_sd, incomming_data_buffer,buffersize, 0);
-    totalBytesRead +=bytes_recieved;
-    if (bytes_recieved == 0){ std::cout<<std::endl << "The server is waiting for your request..!" << std::endl;  close(new_sd);}
-    if (bytes_recieved == -1){ std::cout<<std::endl << "recieve error!" << std::endl;  close(new_sd);}
-
-  for(int i = 0;i<bytes_recieved;i++){
-    std::cout <<incomming_data_buffer[i];
-	fullPath+=incomming_data_buffer[i];
+        cliesize = sizeof(clieaddr);
+        cliefd = accept(socketfd, (struct sockaddr *)&clieaddr, &cliesize);
+        if(cliefd < 0) {
+            perror("accept()");
+            continue;
         }
-}while(bytes_recieved == buffersize);
-     std::cout << totalBytesRead << " bytes recieved" << std::endl;
 
-     staticFiles();
+        int *pcliefd = new int;
+        *pcliefd = cliefd;
+        if(true) {
+            if(pthread_create(&thread, 0, handle_request, pcliefd) < 0) {
+                perror("pthread_create()");
+            } 
+        } else {
+            handle_request(pcliefd);
+        }
+    }
 
-     close(new_sd);
-}
-    std::cout << "Stopping server..." << std::endl;
-    close(socketfd);
-    freeaddrinfo(host_info_list);
-
-return 0 ;
+    return 0;
 }
